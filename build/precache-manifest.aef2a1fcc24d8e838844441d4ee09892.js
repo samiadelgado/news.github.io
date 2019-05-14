967f84e887ef308a1d09c92997c2ad0109e2b8d0
Sami A.Delgado <samiadelgado@gmail.com>
2019-05-14 22:17:18 +0200

    Add files via upload

---
+++ build/precache-manifest.aef2a1fcc24d8e838844441d4ee09892.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68cf5e7a8c779da356c5f360ecd43da4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12a112e05943f7a0e9d6",
+    "url": "/static/css/main.5914bd55.chunk.css"
+  },
+  {
+    "revision": "375d78cc2c4e32624dc7",
+    "url": "/static/js/2.992764e0.chunk.js"
+  },
+  {
+    "revision": "12a112e05943f7a0e9d6",
+    "url": "/static/js/main.b48ccf76.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
